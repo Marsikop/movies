ec0c0b97c27786b90d2c201746a13daec314ef42
Marsikop <roy@gmail.com>
2022-03-31 12:23:36 +0300

    Remove comment

--- src/Movies/Models/MovieHall.cs
+++ src/Movies/Models/MovieHall.cs
@@ -8,7 +8,5 @@ namespace Movies.Models
     {
         public int HallIndex { get; set; }
         public int SeatsCount { get; set; }
-        // public ICollection<ObjectId> Screenings { get; set; }
-        // public Dictionary<DateTime, ICollection<ObjectId>> Screenings { get; set; }   
     }
 }
